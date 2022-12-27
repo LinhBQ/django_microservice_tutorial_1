dad83dba9f2f7ded9d9e51a25dc5565ca2b7c394
linhstartwork@gmail.com <linhstartwork@gmail.com>
2022-12-27 13:19:47 +0700

    update code

---
+++ products/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
+
+# Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass 
+
+    def update(self, request, pk=None):
+        pass 
+
+    def delete(self, request, pk=None):
+        pass
